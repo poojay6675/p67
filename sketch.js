bb334baaf229225c582477f0287e02ce07eeda8a
poojay6675 <78363317+poojay6675@users.noreply.github.com>
2022-08-11 21:16:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var trex, trex_running, trex_collided;
+
+
+function preload(){
+  trex_running=loadAnimation("trex1.png","trex3.png","trex4.png")
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  //create a trex sprite
+  trex=createSprite(50,150,20,50)
+  trex.addAnimation("running",trex_running)
+trex.scale=0.8
+  edges=createEdgeSprites()
+}
+
+function draw() {
+  background(220);
+  if(keyDown("space")){
+    trex.velocityY=-10
+  }
+  trex.velocityY=trex.velocityY+0.5
+  trex.collide(edges[3])
+  drawSprites()
+  
+}
